019873b8ea29cb6d0243cedb2c322a18b7fc7f7d
dadav <33197631+dadav@users.noreply.github.com>
2018-06-11 19:04:22 +0200

    zathura hinzugefügt

--- .bash/settings/misc.sh
+++ .bash/settings/misc.sh
@@ -1,4 +1,4 @@
-#export QTWEBENGINE_DISABLE_SANDBOX=1
+# export QTWEBENGINE_DISABLE_SANDBOX=1
 export QT_IM_MODULE=ibus
 # If not in Tmux and not on laptop
 #( [[ -z "$TMUX" ]] && [[ "$(< /sys/devices/virtual/dmi/id/chassis_vendor)" != "LENOVO" ]] && setleds -D +num || exit 0)
