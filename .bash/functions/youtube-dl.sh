a007e516654f3f442a06a585145bf7abca43df17
dadav <33197631+dadav@users.noreply.github.com>
2018-11-03 20:03:27 +0100

    added parallel

--- .bash/functions/youtube-dl.sh
+++ .bash/functions/youtube-dl.sh
@@ -1,5 +1,16 @@
-
 function animedl() {
   SLEEP_INT=10
   (cd ~/git/youtube-dl || exit 1 && python -m youtube_dl --sleep-interval "$SLEEP_INT" --download-archive ~/.downloaded.txt -o "$HOME/library/videos/anime/%(title)s.%(ext)s" "$@")
+}
+export -f animedl
+
+# Parallel Version
+function panimedl() {
+  # Check if first input is a integer. If yes, use it as thread count.
+  if [[ $1 =~ ^[0-9]+$ ]]; then
+    THREADS=$1
+    shift
+  fi
+
+  parallel --ungroup -j"${THREADS-3}" -- animedl ::: "$@"
 }
