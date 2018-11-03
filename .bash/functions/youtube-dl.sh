bf30d8ce7b4ff9840005d2a09621adbc9322a349
dadav <33197631+dadav@users.noreply.github.com>
2018-11-03 19:02:15 +0100

    update

--- .bash/functions/youtube-dl.sh
+++ .bash/functions/youtube-dl.sh
@@ -1,5 +1,5 @@
 
 function animedl() {
   SLEEP_INT=10
-  (cd ~/git/youtube-dl || exit 1 && python -m youtube_dl --sleep-interval "$SLEEP_INT" --download-archive ~/.downloaded.txt "$HOME/library/videos/anime/%(title)s.%(ext)s" "$@")
+  (cd ~/git/youtube-dl || exit 1 && python -m youtube_dl --sleep-interval "$SLEEP_INT" --download-archive ~/.downloaded.txt -o "$HOME/library/videos/anime/%(title)s.%(ext)s" "$@")
 }
