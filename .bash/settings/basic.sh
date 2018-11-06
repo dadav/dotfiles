4ac49d349449d718a0f122753c0a59d182d88279
dadav <33197631+dadav@users.noreply.github.com>
2018-11-06 17:58:13 +0100

    update

--- .bash/settings/basic.sh
+++ .bash/settings/basic.sh
@@ -1,4 +1,5 @@
-[[ ! ":${HOME}/.local/bin:" =~ ":$PATH:" ]] && export PATH="${HOME}/.local/bin:$PATH" || true
+[[ ! ":$PATH" =~ ":/usr/local/bin:" ]] && export PATH="/usr/local/bin:$PATH" || true
+[[ ! ":$PATH" =~ ":${HOME}/.local/bin:" ]] && export PATH="${HOME}/.local/bin:$PATH" || true
 export PAGER=less
 export EDITOR=nvim
 export VISUAL=$EDITOR
