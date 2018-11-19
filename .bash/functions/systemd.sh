fd5ee5b575b063289f73083c717e3bdf1a7cfa17
Daniel D <33197631+dadav@users.noreply.github.com>
2018-11-19 10:07:24 +0100

    systemd dependencies

--- .bash/functions/systemd.sh
+++ .bash/functions/systemd.sh
@@ -1,8 +1,38 @@
-# Create graph of systemd dependencies
+# Plot systemd dependencies
 function sadot() {
-  if [[ $# -eq 1 ]]; then
-    systemd-analyze dot "$1" 2>/dev/null | dot -Tpng | feh -
-  elif [[ $# -eq 2 ]]; then
-    systemd-analyze dot --from-pattern="$1" --to-pattern="$2" 2>/dev/null | dot -Tpng | feh -
+  ARGS=""
+  FEH=0
+  while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+      -r|--remote)
+        ARGS="$ARGS -H $2"
+        shift
+        shift
+        ;;
+      -f|--from)
+        ARGS="$ARGS --from-pattern=$2"
+        shift
+        shift
+        ;;
+      -t|--to)
+        ARGS="$ARGS --to-pattern=$2"
+        shift
+        shift
+        ;;
+      -o|--open)
+        FEH=1
+        shift
+        ;;
+      *)
+        ARGS="$ARGS $key"
+        shift
+        ;;
+    esac
+  done
+  if [[ $FEH -eq 1 ]]; then
+    systemd-analyze dot $ARGS 2>/dev/null | dot -Tpng | feh -
+  else
+    systemd-analyze dot $ARGS 2>/dev/null | dot -Tpng > out.png
   fi
 }
