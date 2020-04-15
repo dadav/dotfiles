875dd03870d317debb381889828d0d81ba9d3c10
dadav <33197631+dadav@users.noreply.github.com>
2020-04-15 22:31:44 +0200

    update

--- .shells/common/functions/fzf.sh
+++ .shells/common/functions/fzf.sh
@@ -1,4 +1,24 @@
 # PreviewGrep
 function pregre() {
-  vim "$(rg -il "$*" | fzf --preview 'bat --style=numbers --color=always {} | head -500')"
+  F="$(rg -il "$*" | fzf --preview 'bat --style=numbers --color=always {} | head -500')"
+  [[ -n "$F" ]] && nvim "$F"
+}
+
+# select file with fzf; open with given cmd
+function f() {
+  F="$(fzf --preview 'bat --style=numbers --color=always {} | head -500')"
+  [[ -n "$F" ]] && $* "$F"
+}
+
+# open file with nvim
+function fzv() {
+  f nvim
+}
+
+# use fd
+function _fzf_compgen_path() {
+  fd --hidden --follow --exclude ".git" . "$1"
+}
+function _fzf_compgen_dir() {
+  fd --type d --hidden --follow --exclude ".git" . "$1"
 }
---
+++ .shells/zsh/functions/fzf.sh
@@ -0,0 +1,4 @@
+f() {
+    sels=( "${(@f)$(fd "${fd_default[@]}" "${@:2}"| fzf)}" )
+    test -n "$sels" && print -z -- "$1 ${sels[@]:q:q}"
+}
