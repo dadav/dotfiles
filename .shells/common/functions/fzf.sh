d868035b366cf2cb3b0c3e3dcb95f2b7e09a0a1f
dadav <33197631+dadav@users.noreply.github.com>
2020-04-16 16:49:11 +0200

    update

--- .shells/common/functions/fzf.sh
+++ .shells/common/functions/fzf.sh
@@ -4,6 +4,12 @@ function pregre() {
   [[ -n "$F" ]] && nvim "$F"
 }
 
+# git branch
+function fzgb() {
+  B="$(git branch | fzf-tmux -d 15 | tr -d " ")"
+  [[ -n "$B" ]] && git checkout "$B"
+}
+
 # select file with fzf; open with given cmd
 function f() {
   F="$(fzf --preview 'bat --style=numbers --color=always {} | head -500')"
