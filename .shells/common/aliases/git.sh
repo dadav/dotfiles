45031f60dcb7a3c2dc857478f9f8347f594cb1b3
dadav <33197631+dadav@users.noreply.github.com>
2020-04-16 16:49:11 +0200

    update

--- .shells/common/aliases/git.sh
+++ .shells/common/aliases/git.sh
@@ -2,5 +2,3 @@ alias config='/usr/bin/git --git-dir=$HOME/git/dotfiles --work-tree=$HOME'
 alias scripts='/usr/bin/git --git-dir=$HOME/git/scripts --work-tree=$HOME'
 alias gitlog="git log --oneline --decorate --graph --color --all"
 
-# fzf
-alias fzgb='git checkout "$(git branch | fzf-tmux -d 15 | tr -d" ")"'
