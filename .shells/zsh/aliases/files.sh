15c1cd092a1ba56c6dcb3269a14b357be4eb50ff
Daniel D <33197631+dadav@users.noreply.github.com>
2020-04-20 09:01:27 +0200

    add bg function

--- .shells/zsh/aliases/files.sh
+++ .shells/zsh/aliases/files.sh
@@ -1,3 +1,13 @@
+# https://blog.sebastian-daschner.com/entries/zsh-aliases
+# starts one or multiple args as programs in background
+background() {
+  for ((i=2;i<=$#;i++)); do
+    ${@[1]} ${@[$i]} &>/dev/null &
+  done
+}
+
 alias -s {xml,json,yml,yaml}=nvim
-#alias -s {go,py,pp,sh}=code
-alias -s html=chromium
+#alias -s {go,py,pp,sh}="background code"
+alias -s html="background chromium"
+alias -s {pdf,PDF}="background zathura"
+alias -s {zip,ZIP}="unzip -l"
