286381a23e2893c33bc779931e4ccfe7135767fa
dadav <33197631+dadav@users.noreply.github.com>
2020-11-24 19:29:40 +0100

    update

--- .shells/zsh/aliases/globals.sh
+++ .shells/zsh/aliases/globals.sh
@@ -6,7 +6,12 @@ alias -g G='|grep -i'
 alias -g GV='|grep -iva'
 
 # files
+alias -g NDR='**/*(/om[1])'
+alias -g NFR='**/*(.om[1])'
+alias -g BF10R='**/*(.OL[1,10])'
+alias -g OF10R='**/*(.Om[1,10])'
+
 alias -g ND='*(/om[1])'
 alias -g NF='*(.om[1])'
-alias -g BF10='**/*(.OL[1,10])'
-alias -g OF10='**/*(.Om[1,10])'
+alias -g BF10='*(.OL[1,10])'
+alias -g OF10='*(.Om[1,10])'
