848b703b15e250865b1c0be40bc7754cb323966b
dadav <33197631+dadav@users.noreply.github.com>
2020-11-26 17:16:08 +0100

    add some expac aliases

--- .shells/common/aliases/expac.sh
+++ .shells/common/aliases/expac.sh
@@ -1,2 +1,6 @@
-alias lpac='expac -H M "%011m\t%-20n\t%10d" $(comm -23 <(pacman -Qqen | sort) <({ pacman -Qqg base-devel; expac -l "\n" "%E" base; } | sort | uniq)) | sort -n'
-alias lipac='expac --timefmt="%Y-%m-%d %T" "%l\t%n" | sort | tail -n 20'
+# show installed packge sizes
+alias expacl='expac -H M "%m\t%n" | sort -h'
+# show last installed packages
+alias expaci='expac --timefmt="%Y-%m-%d %T" "%l\t%n" | sort | tail -n 20'
+# show dependencies for package
+alias expacd='expac -l "\n" %E -S'
