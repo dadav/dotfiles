852ac317d20861caadb45f6beb5b9b191b1f3796
dadav <33197631+dadav@users.noreply.github.com>
2020-11-26 17:16:08 +0100

    add some expac aliases

--- .shells/zsh/aliases/globals.sh
+++ .shells/zsh/aliases/globals.sh
@@ -1,6 +1,7 @@
-# piping
+# pipeing
 alias -g -- --no-homo='|lolcat'
 alias -g -- --colorize='|ccze -A'
+alias -g L='|less'
 alias -g V='|nvim -'
 alias -g G='|grep -i'
 alias -g GV='|grep -iva'
@@ -15,3 +16,6 @@ alias -g ND='*(/om[1])'
 alias -g NF='*(.om[1])'
 alias -g BF10='*(.OL[1,10])'
 alias -g OF10='*(.Om[1,10])'
+
+# others
+alias -g Q='>/dev/null 2>&1'
