84a9bb7014a5fa37dff8abeaf5fb46702d68d1ee
dadav <33197631+dadav@users.noreply.github.com>
2020-12-12 23:07:52 +0100

    adjust some stuff

---
+++ .shells/common/functions/japanese.sh
@@ -0,0 +1,6 @@
+function hibiki() {
+  num="$(printf "%03d" "$1")"
+  url="https://archive.org/details/hikibiki_podcast/${num}.mp3"
+  notify-send -u normal "Playing Hibiki: ${num}"
+  mpv --profile=podcast "$url" & disown
+}
