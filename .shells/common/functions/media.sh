1fb460fa430b5436a81954a0c606c31d3585308a
dadav <33197631+dadav@users.noreply.github.com>
2020-12-13 10:37:04 +0100

    adjust mpv stuff

--- .shells/common/functions/media.sh
+++ .shells/common/functions/media.sh
@@ -1,13 +1,7 @@
 # Play rainsound
-function rainymood() {
-  FILE=$((RANDOM%4))
-  URL="https://rainymood.com/audio1112/${FILE}.m4a"
-  mpv --volume=70 "$URL" && rainymood
-}
-
-# Search for music and play it in random order
-function fplay() {
-  find ~/music/ -type f -iname "$1" -print0 -exec mpg123 -Z {} +
+function ambient() {
+  notify-send -u normal "Loading playlist"
+  mpv --profile=ambient "https://raw.githubusercontent.com/dadav/naturemood/master/playlist.m3u" &>/dev/null & disown
 }
 
 function nmp3() {
@@ -24,4 +18,9 @@ function vidres() {
   for vid in "$@"; do
     ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of default=nw=1 "$vid"
   done
+}
+
+function ympv {
+  notify-send -u normal "Playing video for \"$*\""
+  (mpv ytdl://ytsearch:"$*" & disown) &>/dev/null
 }
