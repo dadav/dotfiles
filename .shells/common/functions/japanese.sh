c5487d74ef53967649ec302be7f25cfb576f8e63
dadav <33197631+dadav@users.noreply.github.com>
2020-12-13 10:37:04 +0100

    adjust mpv stuff

--- .shells/common/functions/japanese.sh
+++ .shells/common/functions/japanese.sh
@@ -2,5 +2,5 @@ function hibiki() {
   num="$(printf "%03d" "$1")"
   url="https://archive.org/details/hikibiki_podcast/${num}.mp3"
   notify-send -u normal "Playing Hibiki: ${num}"
-  mpv --profile=podcast "$url" & disown
+  (mpv --profile=podcast "$url" & disown) &>/dev/null
 }
