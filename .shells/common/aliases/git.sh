ec76fbcf5b77d6473300885c9146f0841e8d9c4c
dadav <33197631+dadav@users.noreply.github.com>
2021-02-27 19:12:15 +0100

    jump to the git root dir

--- .shells/common/aliases/git.sh
+++ .shells/common/aliases/git.sh
@@ -1,4 +1,4 @@
 alias config='/usr/bin/git --git-dir=$HOME/git/dotfiles --work-tree=$HOME'
 alias scripts='/usr/bin/git --git-dir=$HOME/git/scripts --work-tree=$HOME'
 alias gitlog="git log --oneline --decorate --graph --color --all"
-
+alias gitroot='cd $(git rev-parse --show-toplevel)'
