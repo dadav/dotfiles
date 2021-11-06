290bedb16fcf0fb765bb73a909961475ad56b291
dadav <33197631+dadav@users.noreply.github.com>
2021-11-06 09:18:06 +0100

    Fix modulenotfound

--- .shells/common/settings/python.sh
+++ .shells/common/settings/python.sh
@@ -1,3 +1,4 @@
 export WORKON_HOME=$HOME/python/virtualenvs
 export PYTHONSTARTUP=$HOME/.pythonrc.py
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python
 [[ -s $HOME/.local/bin/virtualenvwrapper.sh ]] && source "$HOME/.local/bin/virtualenvwrapper.sh"
