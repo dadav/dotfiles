bc72d83138bf012fcee779239ea6a248143a834c
dadav <33197631+dadav@users.noreply.github.com>
2022-06-11 09:22:03 +0200

    fix: needs sudo

--- .shells/common/aliases/bpf.sh
+++ .shells/common/aliases/bpf.sh
@@ -1,4 +1,4 @@
 # shellcheck disable=SC2139
-alias sysdig="SYSDIG_BPF_PROBE=$HOME/.sysdig/probe.o sysdig"
-alias csysdig="SYSDIG_BPF_PROBE=$HOME/.sysdig/probe.o csysdig"
-alias falco="FALCO_BPF_PROBE=$HOME/.falco/probe.o falco"
+alias sysdig="sudo SYSDIG_BPF_PROBE=$HOME/.sysdig/probe.o sysdig"
+alias csysdig="sudo SYSDIG_BPF_PROBE=$HOME/.sysdig/probe.o csysdig"
+alias falco="sudo FALCO_BPF_PROBE=$HOME/.falco/probe.o falco"
