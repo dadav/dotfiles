67c2770bb7fdf3ea6a1fb93111186395fb445c46
dadav <33197631+dadav@users.noreply.github.com>
2023-01-26 22:47:16 +0100

    feat: add fcitx configs

--- .shells/common/settings/misc.sh
+++ .shells/common/settings/misc.sh
@@ -1,6 +1,6 @@
-# export QTWEBENGINE_DISABLE_SANDBOX=1
 export QT_IM_MODULE=fcitx
 export GTK_IM_MODULE=fcitx
-export XMODIFIERS=@im=fcitx
-# If not in Tmux and not on laptop
-#( [[ -z "$TMUX" ]] && [[ "$(< /sys/devices/virtual/dmi/id/chassis_vendor)" != "LENOVO" ]] && setleds -D +num || exit 0)
+export XMODIFIERS="@im=fcitx"
+export INPUT_METHOD=fcitx
+# kitty
+export GLFW_IM_MODULE=ibus
