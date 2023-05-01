2def5cf7f498705cbbd51af08da1a24d82b39195
dadav <33197631+dadav@users.noreply.github.com>
2023-05-01 14:24:53 +0200

    fix: also use c-n/c-p to navigate

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -13,8 +13,10 @@ telescope.setup({
 
 		mappings = {
 			i = {
-				["<C-n>"] = actions.cycle_history_next,
-				["<C-p>"] = actions.cycle_history_prev,
+				-- ["<C-n>"] = actions.cycle_history_next,
+				-- ["<C-p>"] = actions.cycle_history_prev,
+				["<C-n>"] = actions.move_selection_next,
+				["<C-p>"] = actions.move_selection_previous,
 
 				["<C-j>"] = actions.move_selection_next,
 				["<C-k>"] = actions.move_selection_previous,
