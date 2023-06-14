5a1f249733e05e07c8c1cfb18448b15712f94c32
dadav <33197631+dadav@users.noreply.github.com>
2023-06-14 20:01:06 +0200

    feat: add helm syntax highlighting

---
+++ .config/nvim/lua/user/filetypes.lua
@@ -0,0 +1,15 @@
+local ft = vim.filetype
+
+-- helm
+ft.add({
+	extension = {
+		tpl = "helm",
+	},
+	filename = {
+		["Chart.yaml"] = "helm",
+	},
+	pattern = {
+		[".*/charts/.*%.yaml"] = "helm",
+		[".*/templates/.*%.yaml"] = "helm",
+	},
+})
