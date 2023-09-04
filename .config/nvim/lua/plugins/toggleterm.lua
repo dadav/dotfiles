cb56dc16806378e1795b3be192d46fb24933c2bd
dadav <33197631+dadav@users.noreply.github.com>
2023-09-04 23:04:01 +0200

    add keymap to start lazygit

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -1,12 +1,14 @@
 local ok, toggleterm = pcall(require, "toggleterm")
 if not ok then
-	return
+  return
 end
 
 toggleterm.setup({
-	open_mapping = [[<a-t>]], -- use alt+t to toggle the terminal
-	direction = "float",
-	float_opts = {
-		border = "curved",
-	},
+  open_mapping = [[<a-t>]], -- use alt+t to toggle the terminal
+  direction = "float",
+  float_opts = {
+    border = "curved",
+  },
 })
+
+vim.api.nvim_set_keymap("n", "<A-g>", "<Cmd>TermExec cmd=lazygit<CR>", { noremap = true, silent = true })
