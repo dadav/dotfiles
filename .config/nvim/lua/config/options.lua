c8c09328a8a0d18d960bb0d8544efaca24e37173
dadav <33197631+dadav@users.noreply.github.com>
2023-09-28 10:45:40 +0200

    fix: remove unneeded files and set some configs

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,17 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+-- use absolute numbers
+opt.relativenumber = false
+
+--  enable mouse support
+opt.mouse = "a"
+
+-- allow cursor to move where there is no text in visual block mode
+opt.virtualedit = "block"
+
+-- german,english
+opt.spelllang = "de,en"
