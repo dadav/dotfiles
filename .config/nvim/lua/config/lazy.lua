494a209c08f80019e09efe7a0e9390ce6d41b21f
dadav <33197631+dadav@users.noreply.github.com>
2023-10-14 16:45:29 +0200

    feat: use LazyExtras

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -11,24 +11,7 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- langs
-    { import = "lazyvim.plugins.extras.lang.go" },
-    { import = "lazyvim.plugins.extras.lang.docker" },
-    { import = "lazyvim.plugins.extras.lang.python" },
-    { import = "lazyvim.plugins.extras.lang.rust" },
-    { import = "lazyvim.plugins.extras.lang.yaml" },
-    { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.lang.ruby" },
-    -- daps
-    { import = "lazyvim.plugins.extras.dap.core" },
-    -- format
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
-    -- misc
-    { import = "lazyvim.plugins.extras.ui.mini-animate" },
-    { import = "lazyvim.plugins.extras.coding.yanky" },
-    { import = "lazyvim.plugins.extras.editor.aerial" },
-    { import = "lazyvim.plugins.extras.ui.edgy" },
-
+    -- INFO: Use `:LazyExtras`
     -- import/override with your plugins
     { import = "plugins" },
   },
