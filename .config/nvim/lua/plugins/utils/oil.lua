0bfb5f296d260b17378c9c306f543494963f628f
dadav <33197631+dadav@users.noreply.github.com>
2023-10-15 16:53:34 +0200

    feat: add oil.nvim

---
+++ .config/nvim/lua/plugins/utils/oil.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    keys = {
+      { "<leader>O", "<CMD>Oil --float<CR>", desc = "Open oil" },
+    },
+    opts = {},
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+}
