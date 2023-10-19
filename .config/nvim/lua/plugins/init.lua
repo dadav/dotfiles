34ab3945b182168fe74956fb8d1a97cb8795c3f4
dadav <33197631+dadav@users.noreply.github.com>
2023-10-19 18:08:07 +0200

    feat: add schreibmaschine.nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -4,11 +4,4 @@ return {
   { import = "plugins.utils" },
   { import = "plugins.lsp" },
   { import = "plugins.cmp" },
-  {
-    "local/schreibmaschine",
-    dev = true,
-    config = true,
-    enabled = true,
-    opts = {},
-  },
 }
