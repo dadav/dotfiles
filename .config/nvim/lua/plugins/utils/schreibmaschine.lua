f5340807d4e50ee18bf2330fc447ec288e7c44f5
dadav <33197631+dadav@users.noreply.github.com>
2024-01-07 19:28:42 +0100

    fix: disable sounds

--- .config/nvim/lua/plugins/utils/schreibmaschine.lua
+++ .config/nvim/lua/plugins/utils/schreibmaschine.lua
@@ -1,12 +1,12 @@
-return {
-  {
-    "dadav/schreibmaschine.nvim",
-    version = false,
-    lazy = false,
-    init = function()
-      vim.keymap.set("n", "<leader>Tt", "<cmd>SchreibmaschineToggle<cr>")
-      vim.keymap.set("n", "<leader>Tp", "<cmd>SchreibmaschineProfilePicker<cr>")
-    end,
-    opts = {},
-  },
-}
+-- return {
+--   {
+--     "dadav/schreibmaschine.nvim",
+--     version = false,
+--     lazy = false,
+--     init = function()
+--       vim.keymap.set("n", "<leader>Tt", "<cmd>SchreibmaschineToggle<cr>")
+--       vim.keymap.set("n", "<leader>Tp", "<cmd>SchreibmaschineProfilePicker<cr>")
+--     end,
+--     opts = {},
+--   },
+-- }
