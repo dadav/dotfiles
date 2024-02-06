12f8a85d54e509c9fa425d081657747b585405ac
dadav <33197631+dadav@users.noreply.github.com>
2024-02-06 19:28:50 +0100

    fix: Use labels

---
+++ .config/nvim/lua/plugins/utils/flash.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "folke/flash.nvim",
+    ---@type Flash.Config
+    opts = {
+      modes = {
+        char = {
+          jump_labels = true,
+        },
+      },
+    },
+  },
+}
