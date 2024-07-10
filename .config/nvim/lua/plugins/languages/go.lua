ceacb86815140c405e9021d45efe7176a7007e0c
dadav <33197631+dadav@users.noreply.github.com>
2024-07-10 13:52:51 +0200

    feat: Support templ

--- .config/nvim/lua/plugins/languages/go.lua
+++ .config/nvim/lua/plugins/languages/go.lua
@@ -1,3 +1,5 @@
+vim.filetype.add({ extension = { templ = "templ" } })
+
 return {
   {
     "jay-babu/mason-nvim-dap.nvim",
@@ -6,5 +8,29 @@ return {
         "delve",
       })
     end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "templ",
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "templ" })
+      end
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        templ = {},
+      },
+    },
   },
 }
