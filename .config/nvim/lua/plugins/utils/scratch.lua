dfe995ad2502ba0c7f5c718c9528d736022db2ba
dadav <33197631+dadav@users.noreply.github.com>
2024-12-01 09:42:37 +0100

    feat: Use scratchpad

---
+++ .config/nvim/lua/plugins/utils/scratch.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "folke/snacks.nvim",
+    keys = {
+      {
+        "<leader>.",
+        function()
+          Snacks.scratch()
+        end,
+        desc = "Toggle Scratch Buffer",
+      },
+      {
+        "<leader>S",
+        function()
+          Snacks.scratch.select()
+        end,
+        desc = "Select Scratch Buffer",
+      },
+    },
+  },
+}
