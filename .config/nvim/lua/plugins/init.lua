8ace072ced9f3684b94c0e7d60f1c420afc1931d
dadav <33197631+dadav@users.noreply.github.com>
2025-04-29 21:11:54 +0200

    adjust nvim config

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -3,5 +3,4 @@ return {
   { import = "plugins.languages" },
   { import = "plugins.utils" },
   { import = "plugins.lsp" },
-  { import = "plugins.cmp" },
 }
--- .config/nvim/lua/plugins/languages/yaml.lua
+++ .config/nvim/lua/plugins/languages/yaml.lua
@@ -8,8 +8,31 @@ return {
     "someone-stole-my-name/yaml-companion.nvim",
     ft = { "yaml" },
     opts = {
+      log_level = "info",
+      formatting = true,
       builtin_matchers = {
         kubernetes = { enabled = true },
+        cloud_init = { enabled = true },
+      },
+      lspconfig = {
+        flags = {
+          debounce_text_changes = 150,
+        },
+        settings = {
+          redhat = { telemetry = { enabled = false } },
+          yaml = {
+            validate = true,
+            format = { enable = true },
+            hover = true,
+            schemaStore = {
+              enable = true,
+              url = "https://www.schemastore.org/api/json/catalog.json",
+            },
+            schemaDownload = { enable = true },
+            schemas = {},
+            trace = { server = "debug" },
+          },
+        },
       },
     },
     dependencies = {
