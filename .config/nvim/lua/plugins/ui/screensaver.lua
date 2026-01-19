709578a10f01b17aa0d96e53fb0de309e163ac9d
dadav <33197631+dadav@users.noreply.github.com>
2026-01-19 19:16:29 +0100

    bump

---
+++ .config/nvim/lua/plugins/ui/screensaver.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "folke/drop.nvim",
+    opts = {
+      screensaver = 1000 * 60 * 5, -- 5 minutes
+    },
+  },
+}
--- .shells/zsh/settings/z.sh
+++ .shells/zsh/settings/z.sh
@@ -1,2 +1,4 @@
-command -v zoxide &>/dev/null && eval "$(zoxide init zsh)"
-alias cd="z"
+if [[ -z "$DISABLE_ZOXIDE" ]]; then
+  command -v zoxide &>/dev/null && eval "$(zoxide init zsh)"
+  alias cd="z"
+fi
